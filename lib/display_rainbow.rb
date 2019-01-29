a461f2a13afab2b1ae0ade9b571504c91ee99b6b
Ryan Bradley <ryanbradley@kpmg.com>
2019-01-29 20:22:35 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,16 @@
+red = "R: red"
+orange = "O: orange"
+yellow = "Y: yellow"
+green = "G: green"
+blue = "B: blue"
+indigo = "I: indigo"
+violet = "V: violet"
+
+
 def display_rainbow(colors)
 
-  puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+  puts "#{colors}"
 
 end
 
-display_rainbow(['red'])
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
